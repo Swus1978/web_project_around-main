d27d7293791625016396c854afa6115c373af6f0
Juan Gustavo Quinones <98677463+Swus1978@users.noreply.github.com>
2025-02-24 13:24:57 -0500

    html update

--- components/Popup.js
+++ components/Popup.js
@@ -1,60 +1,30 @@
 export class Popup {
-  constructor(popupSelector, templateId = null) {
-    if (templateId) {
-      const template = document.querySelector(templateId);
-      if (!template) {
-        throw new Error(`Template with ID "${templateId}" not found.`);
-      }
-      this._popup = template.content.firstElementChild.cloneNode(true);
-      document.body.appendChild(this._popup);
-    } else {
-      this._popup = document.querySelector(popupSelector);
-    }
-
-    if (!this._popup) {
-      throw new Error(`Popup with selector "${popupSelector}" not found.`);
-    }
-
-    this._overlay = document.getElementById('overlay');
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
     this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
-    this._popup.classList.add('popup_opened');
-    if (this._overlay) {
-      this._overlay.style.display = 'block';
-    }
+    this._popup.classList.add('popup--open');
     document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
-    this._popup.classList.remove('popup_opened');
-    if (this._overlay) {
-      this._overlay.style.display = 'none';
-    }
+    this._popup.classList.remove('popup--open');
     document.removeEventListener('keydown', this._handleEscClose);
   }
 
-  _handleEscClose(evt) {
-    if (evt.key === 'Escape') {
+  _handleEscClose(e) {
+    if (e.key === 'Escape') {
       this.close();
     }
   }
 
   setEventListeners() {
-    const closeButton = this._popup.querySelector('.popup__close-button');
-    if (closeButton) {
-      closeButton.addEventListener('click', () => this.close());
-    }
-
-    if (this._overlay) {
-      this._overlay.addEventListener('click', (evt) => {
-        if (evt.target === this._overlay) {
-          this.close();
-        }
-      });
-    }
+    this._popup.addEventListener('click', (e) => {
+      if (e.target.classList.contains('popup__close-button') || e.target === this._popup) {
+        this.close();
+      }
+    });
   }
 }
-
-
--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,38 +1,37 @@
 import { Popup } from './Popup.js';
-export class PopupWithForm extends Popup {
-  constructor(popupSelector, handleFormSubmit) {
-    super(popupSelector);
-    this._handleFormSubmit = handleFormSubmit;
-    this._form = this._popup.querySelector('.form');
-    this._inputList = this._form.querySelectorAll('.form__input');
 
-    if (!this._form) {
-      console.error('Form not found in popup');
-    }
-    if (this._inputList.length === 0) {
-      console.error('No input fields found in form');
-    }
+export class PopupWithForm extends Popup {
+  constructor(popupSelector, handleSubmit) {
+    super(popupSelector);
+    this._handleSubmit = handleSubmit;
+    this._form = this._popup.querySelector('form');
+    this._inputs = this._form.querySelectorAll('.form__input');
+    this._submitButton = this._form.querySelector('.form__submit');
+    this._submitButtonText = this._submitButton.textContent;
   }
 
   _getInputValues() {
-    const inputValues = {};
-    this._inputList.forEach((input) => {
-      inputValues[input.name] = input.value;
+    const values = {};
+    this._inputs.forEach(input => {
+      values[input.name] = input.value;
     });
-    return inputValues;
+    return values;
   }
 
   setEventListeners() {
     super.setEventListeners();
-    this._form.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-      this._handleFormSubmit(this._getInputValues());
-      this.close();
+    this._form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this._handleSubmit(this._getInputValues());
     });
   }
 
   close() {
     super.close();
     this._form.reset();
+  }
+
+  setLoading(isLoading) {
+    this._submitButton.textContent = isLoading ? 'Saving...' : this._submitButtonText;
   }
 }
